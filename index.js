a51b9d08ecd521e61e43a44bd13006a34adaaa24
Johey Hargrett <joheyhargrett@gmail.com>
2024-02-16 16:25:05 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+const headQuarters = 42;
+function distanceFromHqInBlocks(pickup) {
+  if (pickup < 42) {
+    return headQuarters - pickup;
+  } else {
+    return pickup - headQuarters;
+  }
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+  if (destination >= start) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
