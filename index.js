b0d8a22637aaa0a58dd5ce5953a9909717cf3333
Johey Hargrett <joheyhargrett@gmail.com>
2024-03-16 11:16:21 -0400

    made changes to lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    let headQuarters = 42;
+    if(pickUpLocation >= headQuarters) {
+        return pickUpLocation - headQuarters;
+    } else {
+        return headQuarters - pickUpLocation;
+    }
+}
+
+
+
+function distanceFromHqInFeet(pickUpLocation) {
+   let blocks = distanceFromHqInBlocks(pickUpLocation);
+   return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+}
